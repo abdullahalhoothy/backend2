b9812e028ac0c947ad2f4d95840751eca91ae64f
SharifDer <sharifderhem@gmail.com>
2025-03-16 23:18:53 +0300

    user_profile unit test

---
+++ tests/test_user_profile.py
@@ -0,0 +1,19 @@
+import pytest
+from unittest.mock import patch,AsyncMock
+
+
+@pytest.mark.asyncio
+async def test_get_user_profile_endpoint(async_client, user_profile_data , req_load_user_profile):
+
+
+    with ( patch("backend_common.auth.db.get_document", new_callable=AsyncMock) as mock_load_user_profile):
+
+        mock_load_user_profile.return_value =  user_profile_data
+        response = await async_client.post("/fastapi/user_profile",   json=req_load_user_profile)
+        assert response.status_code == 200
+        response_data = response.json()
+        print(response)
+        assert "message" in response_data
+        assert "request_id" in response_data
+        assert "data" in response_data
+
