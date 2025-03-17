44a4f14e0c79209cb791386f01c3336388e4cf5e
SharifDer <sharifderhem@gmail.com>
2025-03-17 22:36:07 +0300

    testing failing_end_case

--- tests/test_user_profile.py
+++ tests/test_user_profile.py
@@ -17,3 +17,12 @@ async def test_get_user_profile_endpoint(async_client, user_profile_data , req_l
         assert "request_id" in response_data
         assert "data" in response_data
 
+@pytest.mark.asyncio
+async def test_user_profile_endpoint_fail(async_client, user_profile_data , req_load_user_profile_duplicate):
+    """" Testing the response when user_id missing """
+
+    with (patch("backend_common.auth.db.get_document" , new_callable=AsyncMock) as mock_load_user_profile):
+        mock_load_user_profile.return_value = user_profile_data
+        response = await async_client.post("/fastapi/user_profile", json=req_load_user_profile_duplicate)
+        assert response.status_code == 422
+
