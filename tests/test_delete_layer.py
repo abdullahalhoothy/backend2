885c1402e54cc62be9f8f5b42872583325a57897
SharifDer <sharifderhem@gmail.com>
2025-04-12 18:11:54 +0300

    Fixing delete Layer ep test

--- tests/test_delete_layer.py
+++ tests/test_delete_layer.py
@@ -15,12 +15,12 @@ async def test_delete_layer(async_client, res_layer_matchings_dataset_matching, 
     req_load_user_profile['request_body']['prdcer_lyr_id'] = 'le2014eaa-2330-4765-93b6-1800edd4979f'
     mock_bg_tasks = MagicMock()
     mock_bg_tasks.add_task = MagicMock()
-    with (patch("storage.db.get_document", new_callable=AsyncMock) as load_dataset,
+    with (patch("storage.db", new_callable=AsyncMock) as load_dataset,
         patch("backend_common.auth.db", new_callable=AsyncMock) as loda_user_profile,
         patch("backend_common.auth.get_background_tasks",return_value = mock_bg_tasks ) as bg_task,
         patch("storage.get_background_tasks", return_value = mock_bg_tasks) as del_bg_task
         ):
-        load_dataset.side_effect = lambda collection, document: get_document_side_effect(
+        load_dataset.get_document.side_effect = lambda collection, document: get_document_side_effect(
             collection, document, 
             res_layer_matchings_user_matching, 
             res_layer_matchings_dataset_matching
@@ -36,12 +36,12 @@ async def test_delete_layer_notfound(async_client, res_layer_matchings_dataset_m
     req_load_user_profile['request_body']['prdcer_lyr_id'] = 'le2014eaa-2330-4765-93b6-1800edd4979f'
     mock_bg_tasks = MagicMock()
     mock_bg_tasks.add_task = MagicMock()
-    with (patch("storage.db.get_document", new_callable=AsyncMock) as load_dataset,
+    with (patch("storage.db", new_callable=AsyncMock) as load_dataset,
         patch("backend_common.auth.db", new_callable=AsyncMock) as loda_user_profile,
         patch("backend_common.auth.get_background_tasks",return_value = mock_bg_tasks ) as bg_task,
         patch("storage.get_background_tasks", return_value = mock_bg_tasks) as del_bg_task
         ):
-        load_dataset.return_value = res_layer_matchings_dataset_matching
+        load_dataset.get_document.return_value = res_layer_matchings_dataset_matching
 
         loda_user_profile.get_document.return_value = user_profile_data
         
@@ -54,12 +54,12 @@ async def test_delete_layer_idmismatch(async_client, res_layer_matchings_dataset
     req_load_user_profile['request_body']['prdcer_lyr_id'] = 'string'
     mock_bg_tasks = MagicMock()
     mock_bg_tasks.add_task = MagicMock()
-    with (patch("storage.db.get_document", new_callable=AsyncMock) as load_dataset,
+    with (patch("storage.db", new_callable=AsyncMock) as load_dataset,
         patch("backend_common.auth.db", new_callable=AsyncMock) as loda_user_profile,
         patch("backend_common.auth.get_background_tasks",return_value = mock_bg_tasks ) as bg_task,
         patch("storage.get_background_tasks", return_value = mock_bg_tasks) as del_bg_task
         ):
-        load_dataset.side_effect = lambda collection, document: get_document_side_effect(
+        load_dataset.get_document.side_effect = lambda collection, document: get_document_side_effect(
             collection, document, 
             res_layer_matchings_user_matching, 
             res_layer_matchings_dataset_matching
